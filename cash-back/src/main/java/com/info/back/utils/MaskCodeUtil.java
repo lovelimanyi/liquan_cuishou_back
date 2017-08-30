8fca73bb6a2be423cb964e5b74d7e375f31a8b06
hexianjun <hxj@xianjinxia.com>
2017-08-30 18:58:06 +0800

    MaskUtil工具类优化

--- cash-back/src/main/java/com/info/back/utils/MaskCodeUtil.java
+++ cash-back/src/main/java/com/info/back/utils/MaskCodeUtil.java
@@ -13,10 +13,13 @@ public class MaskCodeUtil {
     public static String getMaskCode (String code) {
         String maskCode = null;
         if (StringUtils.isNotEmpty(code) && code.length() > 10) {
-            if (code.length() <= 11) {
+            int length = code.length();
+            if (length == 11) {
                 // 手机号码（4-9位以掩码方式显示）
                 maskCode = code.substring(0, 3) + "* * * *" + code.substring(9, code.length());
-            } else {
+            } else if(length < 16){
+                throw new RuntimeException("身份证号码或手机号不正确,请核实：" + code);
+            }else {
                 // 身份证号码（7-15位以掩码方式显示）
                 maskCode =  code.substring(0, 6) + "* * * *" + code.substring(15, code.length());
             }
