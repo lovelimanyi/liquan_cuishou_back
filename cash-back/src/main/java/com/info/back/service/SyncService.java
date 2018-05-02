e3ea5b243124b8042172ea884dd98540fcac6e9a
yangyaofei <yyf@xianjinxia.com>
2018-05-02 23:48:25 +0800

    大额部分还款-处理订单表realMoney字段

--- cash-back/src/main/java/com/info/back/service/SyncService.java
+++ cash-back/src/main/java/com/info/back/service/SyncService.java
@@ -93,6 +93,11 @@ public class SyncService implements ISyncService {
                    if (syncUtils.checkDetailId(idList, detail.getId())){ //判断该详情是否存在，如果不存在则保存
                        CreditLoanPayDetail repaymentDetail = handleRepaymentDetail(detail,payId,loanId);
                        localDataDao.saveCreditLoanPayDetail(repaymentDetail);
+                       //更新订单表，催收流转日志表
+                       HashMap<String,Object> repaymentMap = new HashMap<>();
+                       repaymentMap.put("user_id",userId);
+                       repaymentMap.put("repaymented_amount",repayment.getRealMoney());
+                       syncUtils.updateMmanLoanCollectionOrder(localDataDao,loanId,repaymentMap,Constant.STATUS_OVERDUE_ONE);
                    }
                 }
             }
