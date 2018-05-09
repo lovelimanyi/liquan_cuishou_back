51615d3abd47982e1233f895f05aff6986ee4574
hexianjun <hxj@xianjinxia.com>
2018-05-09 17:57:57 +0800

    ip白名单功能（添加白名单账户）

--- cash-back/src/main/java/com/info/back/controller/UserController.java
+++ cash-back/src/main/java/com/info/back/controller/UserController.java
@@ -1,31 +1,10 @@
 package com.info.back.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.alibaba.fastjson.JSON;
-import jdk.nashorn.internal.runtime.ECMAException;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.ibatis.annotations.Param;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.info.back.service.IBackRoleService;
 import com.info.back.service.IBackUserService;
 import com.info.back.service.ICollectionCompanyService;
-import com.info.back.utils.BackConstant;
-import com.info.back.utils.DwzResult;
-import com.info.back.utils.IdGen;
-import com.info.back.utils.SpringUtils;
-import com.info.back.utils.TreeUtil;
+import com.info.back.utils.*;
 import com.info.constant.Constant;
 import com.info.web.pojo.BackTree;
 import com.info.web.pojo.BackUser;
@@ -33,7 +12,20 @@ import com.info.web.pojo.BackUserCompanyPermissions;
 import com.info.web.pojo.MmanLoanCollectionCompany;
 import com.info.web.util.PageConfig;
 import com.info.web.util.encrypt.MD5coding;
+import org.apache.ibatis.annotations.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("user/")
