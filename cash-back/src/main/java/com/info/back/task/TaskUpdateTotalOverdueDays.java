97a5d062ade102983fbdd57923ebd2c8ebaed9f1
lmy <lmy@xianjinxia.com>
2018-07-04 15:37:59 +0800

    修改bug：改成单线程实现：分期订单入催各期合并

--- cash-back/src/main/java/com/info/back/task/TaskUpdateTotalOverdueDays.java
+++ cash-back/src/main/java/com/info/back/task/TaskUpdateTotalOverdueDays.java
@@ -26,18 +26,16 @@ public class TaskUpdateTotalOverdueDays {
     public void updateTotalOverdueDays() {
         logger.info("更新订单总逾期天数（大订单逾期天数）字段开始... ");
         List<String> loanIds = orderService.getOverdueOrder();
-        System.out.println("111111111111");
-        System.out.println(loanIds);
-        System.out.println("222222222222");
-        ThreadPoolUpdateTotalOverdueDays poolUpdateTotalOverdueDays = ThreadPoolUpdateTotalOverdueDays.getInstance();
-        poolUpdateTotalOverdueDays.setDaemon(true);
+//        ThreadPoolUpdateTotalOverdueDays poolUpdateTotalOverdueDays = ThreadPoolUpdateTotalOverdueDays.getInstance();
+//        poolUpdateTotalOverdueDays.setDaemon(true);
         for (String loanId : loanIds) {
             try {
                 if (StringUtils.isEmpty(loanId)) {
                     continue;
                 }
-                UpdateTotalOverdueDaysThread thread = new UpdateTotalOverdueDaysThread(loanId, orderService);
-                poolUpdateTotalOverdueDays.execute(thread);
+//                UpdateTotalOverdueDaysThread thread = new UpdateTotalOverdueDaysThread(loanId, orderService);
+//                poolUpdateTotalOverdueDays.execute(thread);
+                orderService.updateTotalOverdueDays(loanId);
             } catch (Exception e) {
                 logger.error("更新订单总逾期天数（大订单逾期天数）字段出错，订单id: " + loanId);
                 e.printStackTrace();
